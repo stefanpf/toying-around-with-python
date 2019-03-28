645419988a8b70369008992b953be2dd5fe0ff27
Stefan <stefan@Stefans-Mini.lan>
2019-03-28 16:10:11 +0100

    make things more universal by moving more things into environment variables

--- 02-website-status-checker/monitor.py
+++ 02-website-status-checker/monitor.py
@@ -9,17 +9,20 @@ EMAIL_ADDRESS_SENDER = os.environ.get('EMAIL_ADDRESS_SENDER')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_ADDRESS_RECEIVER = os.environ.get('EMAIL_ADDRESS_RECEIVER')
 MONITOR_URL = os.environ.get('MONITOR_URL')
+SMTP_SERVER = os.environ.get('SMTP_SERVER')
 
 response = requests.get(MONITOR_URL, timeout=5)
 
-if response.status_code != 200:
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
-        smtp.login(EMAIL_ADDRESS_SENDER, EMAIL_PASSWORD)
+# if response.status_code != 200:
+with smtplib.SMTP(SMTP_SERVER, 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(EMAIL_ADDRESS_SENDER, EMAIL_PASSWORD)
 
-        subject = f'{MONITOR_URL} IS DOWN!'
-        body = 'GET request failed, status code != 200.'
-        msg = f'Subject: {subject}\n\n{body}'
-        smtp.sendmail(EMAIL_ADDRESS_SENDER, EMAIL_ADDRESS_RECEIVER, msg)
+    subject = f'{MONITOR_URL} IS DOWN!'
+    body = f"""GET request failed, status code != 200.\n\n
+           Received status code: {response.status_code}
+            """
+    msg = f'Subject: {subject}\n\n{body}'
+    smtp.sendmail(EMAIL_ADDRESS_SENDER, EMAIL_ADDRESS_RECEIVER, msg)
